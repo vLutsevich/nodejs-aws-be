3bb75ae6018e4687f726b220576bf2cdf6383a7e
Valery Lutsevich <tsubasa@tut.by>
2020-11-02 10:19:59 +0300

    task 3: async await and try catch

--- product-service/src/getProductById.ts
+++ product-service/src/getProductById.ts
@@ -1,13 +1,25 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
-import { productList } from './mock/productList';
+import { productList } from './productList';
+import { timeout } from './timeout';
 
 export const getProductById: APIGatewayProxyHandler = async (event) => {
-  const { productId } = event.pathParameters;
-  const productIndex = productList.findIndex(x => x.id === productId);
+  try {
+    await timeout(100);
+    const { productId } = event.pathParameters;
+    const productIndex = productList.findIndex(x => x.id === productId);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(productList[productIndex])
-  };
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Methods": "OPTIONS,GET",
+        "Access-Control-Allow-Origin": "https://d3bq2tyrxffjd4.cloudfront.net",
+      },
+      body: JSON.stringify(productList[productIndex])
+    };
+  } catch(e) {
+    console.log(e);
+  }
+
 }
--- product-service/src/getProductList.ts
+++ product-service/src/getProductList.ts
@@ -1,15 +1,21 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
-import { productList } from './mock/productList';
+import { productList } from './productList';
+import { timeout } from './timeout';
 
 export const getProductList: APIGatewayProxyHandler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-      "Access-Control-Allow-Methods": "OPTIONS,GET",
-      "Access-Control-Allow-Origin": "https://d3bq2tyrxffjd4.cloudfront.net",
-    },
-    body: JSON.stringify(productList)
-  };
+  try {
+    await timeout(100);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Methods": "OPTIONS,GET",
+        "Access-Control-Allow-Origin": "https://d3bq2tyrxffjd4.cloudfront.net",
+      },
+      body: JSON.stringify(productList)
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
